fb0391c9fa5f16b645b0cd4bb2b3c3f29ae4315f
OlguD <olgudegirmenci34@gmail.com>
2024-11-04 11:20:58 +0200

    face detection implemented

--- video_capture.py
+++ video_capture.py
@@ -1,18 +1,26 @@
 import cv2 as cv
+from detect_face import detect_face
 
-def capture_video(device: str='mac'):
-    # 0 means default camera
+def capture_video(device: str = 'mac'):
+    # 0 varsayılan kamerayı temsil eder
     if device == 'win':
         num = 0
+    else:
+        num = 1
 
-    camera = cv.VideoCapture(1)
+    camera = cv.VideoCapture(num)  # Kamera indeksini kontrol et (0 veya 1 olabilir)
 
     while True:
         check, frame = camera.read()
         if not check:
             break
-        frame = cv.flip(frame, 1)
-        cv.imshow("Camera", frame)
+
+        # Yüz algılama fonksiyonunu uygula
+        frame_with_faces = detect_face(frame)
+
+        # Çerçeveyi yansıt (gerekirse)
+        frame_with_faces = cv.flip(frame_with_faces, 1)
+        cv.imshow("Camera - Detected Faces", frame_with_faces)
 
         key = cv.waitKey(1)
         if key == ord('q'):
